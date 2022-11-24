9a7522f69e6b5e290c81fa446059abdba368c5d5
Pankaj Sarkar <everythingcivilin@gmail.com>
2022-11-24 23:20:04 +0530

    Updating 2.0

--- dist/_nuxt/static/1642701378/projects/kapil-agarwal/36/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1642701378",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprojects\u002Fkapil-agarwal\u002F36",config:{baseUrl:"https:\u002F\u002Fnovusarcdesign.com\u002F",_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
--- dist/_nuxt/static/1642701378/projects/ssipl-shyam-vatika/62/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1642701378",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprojects\u002Fssipl-shyam-vatika\u002F62",config:{baseUrl:"https:\u002F\u002Fnovusarcdesign.com\u002F",_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
